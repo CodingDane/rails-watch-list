e2b98605cb08802cebc4abee6da905a1c8d55581
alexander voler <avoeler@gmail.com>
2024-03-22 08:16:21 +0900

    changed strong params

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -34,6 +34,6 @@ class BookmarksController < ApplicationController
   private
 
   def bookmark_params
-    params.require(:bookmark).permit(:comment, :movie_id)
+    params.require(:wa_li_bookmark).permit(:comment, :movie_id)
   end
 end
