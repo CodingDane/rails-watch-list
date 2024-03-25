746a540a3b1fe5d7873dd891203db2af61b729ef
alexander voler <avoeler@gmail.com>
2024-03-25 14:16:23 +0900

    authenticate user, list belongs to user

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,6 +2,7 @@ class List < ApplicationRecord
   self.table_name = "wa_li_lists"
   has_many :bookmarks, dependent: :destroy, foreign_key: "wa_li_list_id"
   has_many :movies, through: :bookmarks, foreign_key: "wa_li_list_id"
+  belongs_to :user, foreign_key: "wa_li_user_id"
 
   validates :name, presence: true, uniqueness: true
   has_one_attached :photo
