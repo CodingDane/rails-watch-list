cb7e4b7c01b207073fd21ce9c175885cbed6de51
alexander voler <avoeler@gmail.com>
2024-03-26 19:14:52 +0900

    almost works on local - changed list controller a bit

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -13,7 +13,7 @@ class ListsController < ApplicationController
     @list = List.new(list_params)
     @list.user = current_user
     if @list.save
-      redirect_to lists_path
+      redirect_to list_path(@list)
     else
       render :new, status: unprocessable_entity
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_16_045437) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_26_100515) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -42,33 +42,48 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_16_045437) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
-  create_table "bookmarks", force: :cascade do |t|
+  create_table "wa_li_bookmarks", force: :cascade do |t|
     t.string "comment"
+    t.bigint "wa_li_movie_id", null: false
+    t.bigint "wa_li_list_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "list_id", null: false
-    t.bigint "movie_id", null: false
-    t.index ["list_id"], name: "index_bookmarks_on_list_id"
-    t.index ["movie_id"], name: "index_bookmarks_on_movie_id"
+    t.index ["wa_li_list_id"], name: "index_wa_li_bookmarks_on_wa_li_list_id"
+    t.index ["wa_li_movie_id"], name: "index_wa_li_bookmarks_on_wa_li_movie_id"
   end
 
-  create_table "lists", force: :cascade do |t|
+  create_table "wa_li_lists", force: :cascade do |t|
     t.string "name"
+    t.bigint "wa_li_user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["wa_li_user_id"], name: "index_wa_li_lists_on_wa_li_user_id"
   end
 
-  create_table "movies", force: :cascade do |t|
+  create_table "wa_li_movies", force: :cascade do |t|
     t.string "title"
     t.string "overview"
-    t.string "poster_url"
+    t.string "posterurl"
     t.float "rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
+  create_table "wa_li_users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_wa_li_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_wa_li_users_on_reset_password_token", unique: true
+  end
+
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "bookmarks", "lists"
-  add_foreign_key "bookmarks", "movies"
+  add_foreign_key "wa_li_bookmarks", "wa_li_lists"
+  add_foreign_key "wa_li_bookmarks", "wa_li_movies"
+  add_foreign_key "wa_li_lists", "wa_li_users"
 end
