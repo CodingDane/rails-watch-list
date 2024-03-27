f7166e25150b28d4000dfb2fb4904d33d5d129db
alexander voler <avoeler@gmail.com>
2024-03-27 15:34:41 +0900

    more changes in bookmarks controller

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -11,12 +11,12 @@ class BookmarksController < ApplicationController
 
   def new
     @bookmark = Bookmark.new
-    @list = List.find(params[:wa_li_list_id])
+    @list = List.find(params[:list_id])
   end
 
   def create
     @bookmark = Bookmark.new(bookmark_params)
-    @list = List.find(params[:wa_li_list_id])
+    @list = List.find(params[:list_id])
 
     @bookmark.list = @list
 
