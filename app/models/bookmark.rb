8589e9f768f9f2b834b32c318bdcee02eb25075b
alexander voler <avoeler@gmail.com>
2024-03-27 15:24:22 +0900

    naming error of movie and list foreign keys in bookmark model

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,7 +1,7 @@
 class Bookmark < ApplicationRecord
   self.table_name = "wa_li_bookmarks"
-  belongs_to :movie, foreign_key: "lw_cine_movie_id"
-  belongs_to :list, foreign_key: "lw_cine_list_id"
+  belongs_to :movie, foreign_key: "wa_li_movie_id"
+  belongs_to :list, foreign_key: "wa_li_list_id"
 
   validates :comment, presence: true, length: { minimum: 6 }
   validates :movie, uniqueness: { scope: :wa_li_list }
